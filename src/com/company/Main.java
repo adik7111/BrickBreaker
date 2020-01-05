b6a80f3ad5a0893c371a2a124df4154a1d282f5a
K1631110 <adrian.kucharski@kcl.ac.uk>
2020-01-05 16:03:25 +0000

    Added gameplay as panel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    JFrame frame = new JFrame();
+	    Gameplay gp = new Gameplay();
 	    frame.setBounds(10, 10, 700, 600);
 	    frame.setTitle("Brick Breaker");
 	    frame.setResizable(false);
 	    frame.setVisible(true);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.add(gp);
     }
 }
