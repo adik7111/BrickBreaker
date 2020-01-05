cf21e77f5ba79bce2e2ef13be1174684437c7d48
K1631110 <adrian.kucharski@kcl.ac.uk>
2020-01-05 15:32:10 +0000

    Set-up Gameplay class

---
+++ src/com/company/Gameplay.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
