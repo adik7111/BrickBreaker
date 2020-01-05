c9a63531b48ed5ed3ac07f63819cb14b89f3e7fb
K1631110 <adrian.kucharski@kcl.ac.uk>
2020-01-05 16:03:09 +0000

    Added multiple functionalities.
    Paint method which creates the paddle and ball
    Constructor which adds key listener
    Move left and right method
    on key press moves left or right.

--- src/com/company/Gameplay.java
+++ src/com/company/Gameplay.java
@@ -1,15 +1,75 @@
 package com.company;
 
 import javax.swing.JPanel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.Timer;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean isPlaying = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer time;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+
+    private int ballDirX = -1;
+    private int ballDirY = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        time = new Timer(delay, this);
+        time.start();
+    }
+
+    public void moveRight() {
+        isPlaying = true;
+        playerX+= 20;
+    }
+
+    public void moveLeft() {
+        isPlaying = true;
+        playerX-= 20;
+    }
+
+    public void paint(Graphics g) {
+        //background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        //borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(681, 0, 3, 592);
+
+        //paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        //ball
+        g.setColor(Color.yellow);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+
+        g.dispose();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        time.start();
+        repaint();
     }
 
     @Override
@@ -19,7 +79,19 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
     }
 
     @Override
