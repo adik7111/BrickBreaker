de601092cdc408d4fa23b4b60d23acf8e3f51b97
K1631110 <adrian.kucharski@kcl.ac.uk>
2020-01-05 15:26:11 +0000

    Added frame on execution.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    JFrame frame = new JFrame();
+	    frame.setBounds(10, 10, 700, 600);
+	    frame.setTitle("Brick Breaker");
+	    frame.setResizable(false);
+	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
